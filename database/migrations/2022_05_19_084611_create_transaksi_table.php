0c68d79ed30f1224c1f8c21f894a265df0c83d48
endi julian <endi@iglobal.co.id>
2022-05-19 20:47:02 +0700

    migration table transaksi and transaksi_status

---
+++ database/migrations/2022_05_19_084611_create_transaksi_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransaksiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transaksi', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode');
+            $table->string('deskripsi');
+            $table->date('tanggal');
+            $table->string('nilai');
+            $table->integer('dompet_id');
+            $table->integer('kategori_id');
+            $table->integer('status_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transaksi');
+    }
+}
